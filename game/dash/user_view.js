b48630aa2cf2790c3fc0fa0d692aa47f2b46e18a
Srowyesha Deshar <70159885@nebraska.edu>
2026-06-18 15:44:31 -0500

    Updated dashboard and added missions/ debugged the codes/ Last update 6/18/2026

--- game/dash/user_view.js
+++ game/dash/user_view.js
@@ -1,8 +1,38 @@
-//index.js
+let pages=document.querySelectorAll(".page");
 
-let menuicn = document.querySelector(".menuicn");
-let nav = document.querySelector(".navcontainer");
+function showPage(id){
+    pages.forEach(page=>{
+        page.classList.remove("active");
+    });
 
-menuicn.addEventListener("click", () => {
-    nav.classList.toggle("navclose");
-})
+    document
+    .getElementById(id)
+    .classList.add("active");
+}
+
+document
+.getElementById("dashboard-btn")
+.onclick=()=>showPage("dashboard-page");
+
+document
+.getElementById("scores-btn")
+.onclick=()=>showPage("scores-page");
+
+document
+.getElementById("progress-btn")
+.onclick=()=>showPage("progress-page");
+
+document
+.getElementById("news-btn")
+.onclick=()=>showPage("news-page");
+
+document
+.getElementById("profile-btn")
+.oncick=()=>showPage("profile-page");
+
+
+document
+.getElementById("game1")
+.addEventListener("click", function () {
+    window.location.href="../missions/game1.html";
+});
